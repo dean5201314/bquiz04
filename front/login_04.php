348c9440c34f022ba4016bfbdc1b04ba81dd200f
dean5201314 <smile90624@gmail.com>
2024-01-22 16:14:10 +0800

    完成管理登入功能+修正表單password欄位設定

--- front/login_04.php
+++ front/login_04.php
@@ -1,4 +1,7 @@
-<h2>管理員登入</h2>
+<h2>第一次購買</h2>
+<img src="./icon/0413.jpg" onclick="location.href='?do=reg'">
+
+<h2>會員登入</h2>
 <!-- table.all>tr*3>td.tt.ct+td.pp>input:text -->
 <table class="all">
     <tr>
@@ -7,7 +10,7 @@
     </tr>
     <tr>
         <td class="tt">密碼</td>
-        <td class="pp"><input type="password" name="pw" id="pw"></td>
+        <td class="pp"><input type="text" name="pw" id="pw"></td>
     </tr>
     <tr>
         <td class="tt">驗證碼</td>
@@ -21,22 +24,20 @@
             <input type="text" name="ans" id="ans"></td>
     </tr>
 </table>
-<div class="ct"><button onclick="login('admin')">確認</button></div>
+<div class="ct">
+    <button onclick="login('mem')">確認</button>
+</div>
 <script>
 function login(table){
     $.get('./api/chk_ans.php',{ans:$("#ans").val()},(chk)=>{
         if(parseInt(chk)==0){
-            alert("對不起，您輸入的驗證碼有誤，請您重新登入")
+            alert("驗證碼錯誤，請重新輸入")
         }else{
-            $.post("./api/chk_pw.php",
-                    {table,
-                     acc:$("#acc").val(),
-                     pw:$("#pw").val()},
-                    (res)=>{
+            $.post("./api/chk_pw.php",{table,acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
                 if(parseInt(res)==0){
                     alert("帳號或密碼錯誤，請重新輸入")
                 }else{
-                    location.href='back.php?do=admin';
+                    location.href='index.php';
                 }
             })
         }
